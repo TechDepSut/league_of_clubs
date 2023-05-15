c0c0e2242a97be776abc3b9ea07fb5fb9e12d26e
kill-your-soul <Platonovdi@gmail.com>
2023-05-15 23:40:32 +0300

    add models for student, club and winner

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Club, Winner, Student
+
+admin.site.site_header = "League of clubs"
+admin.site.site_title = "League of clubs"
 
 # Register your models here.
+
+admin.site.register(Club)
+admin.site.register(Winner)
+admin.site.register(Student)
