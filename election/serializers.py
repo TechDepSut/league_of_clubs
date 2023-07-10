967cef8ece38cf33bdab2aaab9d02d7ec1695615
Lordzoris <nasca@mail.ru>
2023-07-10 22:36:06 +0300

    Update from main

--- election/serializers.py
+++ election/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
-from .models import Club, Student
+from .models import Club, Student, Winner
 
 
 class ClubSerializer(serializers.ModelSerializer):
     class Meta:
         model = Club
-        fields = "__all__"
-
+        fields = '__all__'
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = "__all__"
+        fields = ['uid', 'group_number', 'faculty', 'first_name', 'last_name', 'patronymic', 'student_credit', 'course', 'training_form', 'age', 'email']
+
+class WinnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Winner
+        fields = '__all__'
--- election/urls.py
+++ election/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import ClubViewSet, StudentViewSet
+from api import views
 
 router = routers.DefaultRouter()
-router.register(r"clubs", ClubViewSet)
-router.register(r"students", StudentViewSet)
+router.register(r'clubs', views.ClubViewSet)
+router.register(r'students', views.StudentViewSet)
+router.register(r'winners', views.WinnerViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
