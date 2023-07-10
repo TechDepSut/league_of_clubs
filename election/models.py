2ddd22134503752bf07a03fedb272e390e29fe48
Lordzoris <nasca@mail.ru>
2023-07-10 22:32:50 +0300

    Update models from main

--- election/models.py
+++ election/models.py
@@ -2,20 +2,40 @@ from django.db import models
 
 
 class Club(models.Model):
+
+    class Statuses(models.TextChoices):
+        ON_ELECTION = 'on_election', 'On election'
+        INACTIVE = 'inactive', 'Inactive'
+        WINNER = 'winner', 'Winner'
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    logo = models.ImageField(upload_to="clubs/", null=True, blank=True)
-    votes = models.PositiveIntegerField(default=0)
+    logo = models.ImageField(upload_to='clubs', blank=True, null=True)
+    status = models.CharField(max_length=20, choices=Statuses.choices, default=Statuses.INACTIVE)
 
-    def __str__(self):
-        return self.name
+
+class Winner(models.Model):
+    id = models.AutoField(primary_key=True)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    season = models.IntegerField()
 
 
 class Student(models.Model):
-    uid = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    choice = models.ForeignKey(Club, on_delete=models.CASCADE, null=True, blank=True)
-    choice2 = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.name
+    class TrainingForm(models.TextChoices):
+        FULL_TIME = 'full_time', 'Очная'
+        DISTANCE = 'distance', 'Заочная'
+        FULL_TIME_AND_DISTANCE = 'full_time_and_distance', 'Очно-заочная'
+
+    uid = models.IntegerField()
+    group_number = models.CharField(max_length=10)
+    faculty = models.CharField(max_length=6)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    patronymic = models.CharField(max_length=30)
+    student_credit = models.IntegerField() # номер зачетки
+    course = models.IntegerField()
+    training_form = models.CharField(max_length=25, choices=TrainingForm.choices, default=TrainingForm.FULL_TIME)
+    age = models.IntegerField()
+    email = models.EmailField()
+    choise = models.BooleanField(default=False)
