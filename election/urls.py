be38626cadb68058f367bf4c15a17ae9500a0132
Lordzoris <nasca@mail.ru>
2023-09-02 02:27:17 +0300

    a little bit changed, but not working

--- election/urls.py
+++ election/urls.py
@@ -10,24 +10,9 @@ router.register(r'clubs', ClubViewSet)
 router.register(r'students', StudentViewSet)
 router.register(r'winners', WinnerViewSet)
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="API",
-      default_version='v1',
-      description="API documentation",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@league_of_clubs.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-    permission_classes=(
-        permissions.IsAuthenticated,
-    ),
-)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('', include(router.urls)),
 ]
