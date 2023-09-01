d710de553ea34f4054870deeb1943646ce707e4f
Lordzoris <nasca@mail.ru>
2023-09-02 02:26:36 +0300

    a little bit changed, but not working

--- api/urls.py
+++ api/urls.py
@@ -18,15 +18,14 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
-    permission_classes=(
+    permission_classes=[
         permissions.IsAuthenticated,
-    ),
+    ],
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include(router.urls)),
 ]
